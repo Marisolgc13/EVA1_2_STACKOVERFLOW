7435267cceac8df06556e8e1bb735c1605c2869d
Marisolgc13 <64939683+Marisolgc13@users.noreply.github.com>
2023-03-05 17:36:57 -0700

    Add files via upload

---
+++ EVA1_2_STACKOVERFLOW/src/eva1_2_stackoverflow/EVA1_2_STACKOVERFLOW.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_stackoverflow;
+
+/**
+ *
+ * @author Marisol G
+ */
+public class EVA1_2_STACKOVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // TODO code application logic here
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        A();
+    }
+    
+    
+}
